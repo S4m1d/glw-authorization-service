f00fe21953933cfa6bc8ebb3aadd455887182e4c
Dmitry Kolankov <kolankovda@student.bmstu.ru>
2022-11-01 22:53:40 +0600

    added unit tests

---
+++ business-logic-impl/src/test/java/com/s4m1d/glw/authorization/service/business/logic/AccountCreationServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.s4m1d.glw.authorization.service.business.logic;
+
+import com.s4m1d.glw.authorization.service.business.logic.constant.AccountCreationResult;
+import com.s4m1d.glw.authorization.service.business.logic.datamodel.AccountCreationInfo;
+import com.s4m1d.glw.authorization.service.dba.AccountDbaService;
+import com.s4m1d.glw.authorization.service.dba.exception.AccountAlreadyExistsException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
+public class AccountCreationServiceImplTest {
+    private AccountCreationServiceImpl accountCreationService;
+    private AccountDbaService accountDbaService;
+
+    @BeforeMethod
+    public void setup() {
+        accountDbaService = mock(AccountDbaService.class);
+        accountCreationService = new AccountCreationServiceImpl(accountDbaService);
+    }
+
+    @Test
+    public void createAccount_creation_success() {
+        AccountCreationResult accountCreationResult;
+        try {
+            accountCreationResult = accountCreationService.createAccount(new AccountCreationInfo());
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+
+        Assert.assertEquals(accountCreationResult, AccountCreationResult.CREATION_SUCCESS);
+    }
+
+    @Test
+    public void createAccount_already_exists() throws AccountAlreadyExistsException {
+        doThrow(new AccountAlreadyExistsException()).when(accountDbaService).createAccount(any(), any());
+
+        AccountCreationResult accountCreationResult;
+        try {
+            accountCreationResult = accountCreationService.createAccount(new AccountCreationInfo());
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+
+        Assert.assertEquals(accountCreationResult, AccountCreationResult.ALREADY_EXISTS);
+    }
+
+    @Test
+    public void createAccount_other_exception() throws AccountAlreadyExistsException {
+        doThrow(new RuntimeException("some msg")).when(accountDbaService).createAccount(any(), any());
+
+        AccountCreationResult accountCreationResult;
+        try {
+            accountCreationResult = accountCreationService.createAccount(new AccountCreationInfo());
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+
+        Assert.assertEquals(accountCreationResult, AccountCreationResult.OTHER_EXCEPTION);
+    }
+}
